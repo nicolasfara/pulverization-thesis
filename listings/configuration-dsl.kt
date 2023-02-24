a89996658b0139c2d302fdad17121b83d9f84e64
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-02-24 16:46:17 +0100

    docs: fix image positions

--- listings/configuration-dsl.kt
+++ listings/configuration-dsl.kt
@@ -5,17 +5,14 @@ val config = pulverizationConfig {
         CommunicationComponent deployableOn Edge
         SensorsComponent deployableOn Device
     }
-
     logicalDevice("device-2") {
         BehaviourComponent and StateComponent and CommunicationComponent deployableOn Device
     }
-
     logicalDevice("device-3") {
         BehaviourComponent deployableOn Cloud
         CommunicationComponent deployableOn Edge
         SensorsComponent deployableOn Device
     }
-
     deviceLinks {
         "device-1" linkedWith "device-2"
         "device-1" linkedWith "device-3"
